b76cd03273d57c9f69a1c52a3f81b02648421bc2
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-15 16:28:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ovning2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+
+            Console.WriteLine("Skriv ett heltal:  ");
+
+            x = int.Parse(Console.ReadLine());
+            
+            if ( x % 2 == 0)
+            {
+
+                Console.WriteLine("{0} är jämnt!", x);
+            }
+            else
+            {
+                Console.WriteLine("{0} är udda!!", x);
+
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
